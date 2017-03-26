3f55862b64ddc945db65cac36d25c6114681835a
ZbigniewS <zbigniewsitarz@gmail.com>
2017-03-26 22:21:39 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// plik scripts.js
+
+var name = prompt('Wpisz swoje imię');
+// var country = prompt('Wpisz kraj')
+var month = prompt('Podaj obecny miesiąc')
+/* 
+deactivity text info
+alert('Witaj, ' + name + ' twój kraj to ' +  country);
+console.log('Witaj, ' + name + ' twój kraj to ' +  country);
+*/
+alert('Witaj, ' + name + '  w miesiącu ' +  month);
+console.log('Witaj, ' + name + ' w miesiącu ' +  month);
